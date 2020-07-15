40a6b39e289e1bf32089eb32093908e763e8e2b9
Dan Dumitriu <dandumitriu33@gmail.com>
2020-07-15 13:09:23 +0300

    Add initial Models fields/properties

---
+++ Parkopolis/Parkopolis.API/Models/ParkingSpaceDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parkopolis.API.Models
+{
+    public class ParkingSpaceDto
+    {
+    }
+}
