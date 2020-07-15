cdad99b5b6ba4efe399b6befb09a6a6adc969825
Dan Dumitriu <dandumitriu33@gmail.com>
2020-07-15 14:02:22 +0300

    Add API Areas Get Requests

--- Parkopolis/Parkopolis.API/Controllers/AreasController.cs
+++ Parkopolis/Parkopolis.API/Controllers/AreasController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Parkopolis.API.MockData;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +7,45 @@ using System.Threading.Tasks;
 
 namespace Parkopolis.API.Controllers
 {
+    [ApiController]
+    [Route("api/cities/{cityId}/areas")]
     public class AreasController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult GetAreas(int cityId)
+        {
+            var city = CitiesDataStore.CurrentCities.Cities
+                .FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(AreasDataStore.CurrentAreas.Areas.FindAll( a => a.CityId == cityId));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Getarea(int cityId, int id)
+        {
+            // find city
+            var cityToReturn = CitiesDataStore.CurrentCities.Cities
+                .FirstOrDefault(c => c.Id == cityId);
+
+            if (cityToReturn == null)
+            {
+                return NotFound();
+            }
+            //if (cityId !=)
+            //{
+
+            //}
+            var result = AreasDataStore.CurrentAreas.Areas.FindAll(a => a.Id == id && cityId == a.CityId);
+            if (result.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
     }
 }
