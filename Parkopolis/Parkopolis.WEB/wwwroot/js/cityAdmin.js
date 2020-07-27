a62b58fdf78f8e075d7d5bf0b955c2e0e3f91bde
Dan Dumitriu <dandumitriu33@gmail.com>
2020-07-27 22:30:40 +0300

    Add JS to populate cities on CityAdmin page

---
+++ Parkopolis/Parkopolis.WEB/wwwroot/js/cityAdmin.js
@@ -0,0 +1,21 @@
+﻿let citiesList = []
+$.getJSON('http://localhost:1028/api/cities', function (data) {
+    for (var i = 0; i < data.length; i++) {
+        citiesList.push(data[i]);
+        
+    }
+    console.log(citiesList);
+    populateCities();
+});
+
+function populateCities() {
+    for (var i = 0; i < citiesList.length; i++) {
+        $(".citiesContainer").append(
+            `
+            <p>ID: ${citiesList[i].id}</p>
+            <p>Name: ${citiesList[i].name}</p>
+            `
+        );
+    }
+}
+
