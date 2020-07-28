cfc0efda44358a6b0b4b931af4bec4f5730e5e1c
Dan Dumitriu <dandumitriu33@gmail.com>
2020-07-28 11:12:43 +0300

    Add API POST create new parking space

---
+++ Parkopolis/Parkopolis.WEB/wwwroot/js/parkingAdmin.js
@@ -0,0 +1,37 @@
+﻿console.log("eet works");
+
+$("#newParkingSpaceSubmit").click(async function () {
+    let parkingLotId = await $("h3").first().attr("id").replace("parkingLotId", "");
+    //alert(parkingLotId);
+    let parkingSpaceName = await $("#parkingSpaceName").val();
+    //alert(parkingSpaceName);
+
+
+    let data = {
+        "parkingLotId": parkingLotId,
+        "name": parkingSpaceName,
+        "isTaken": false,
+        "hasCarWash": true,
+        "isCovered": true,
+        "price": 10.0,
+        "details": "Close to exit."
+    };
+    
+
+    $.ajax({
+        type: "POST",
+        url: `http://localhost:1028/api/cities/1/areas/1/parkinglots/${parkingLotId}/parkingspaces`,
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8",
+        crossDomain: true,
+        dataType: "json",
+        success: function () {
+            alert("success");
+        },
+        error: function (jqXHR, status) {
+            // error handler
+            console.log(jqXHR);
+            alert('fail' + status.code);
+        }
+    })
+});
