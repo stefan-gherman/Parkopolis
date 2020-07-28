7fa4da76982690d0660b8a8f47b8cd38812a3820
Dan Dumitriu <dandumitriu33@gmail.com>
2020-07-28 12:11:03 +0300

    Add JQuery City population on CityAdmin sections

--- Parkopolis/Parkopolis.WEB/wwwroot/js/cityAdmin.js
+++ Parkopolis/Parkopolis.WEB/wwwroot/js/cityAdmin.js
@@ -1,20 +1,7 @@
-﻿let citiesList = []
-$.getJSON('http://localhost:1028/api/cities', function (data) {
-    for (var i = 0; i < data.length; i++) {
-        citiesList.push(data[i]);     
-    }
-    populateCities();
-});
+﻿let allCities = []
+getCitiesFromDb();
 
-function populateCities() {
-    for (var i = 0; i < citiesList.length; i++) {
-        $("#citiesContainer").append(
-            `<p id="city${citiesList[i].id}">${citiesList[i].name}</p>  
-            `
-        );
-    }
-}
-
+// Submitting a new city
 $("#newCitySubmit").click(function () {
     console.log($("#cityName").val());
     let newCityName = $("#cityName").val();
@@ -40,3 +27,65 @@ $("#newCitySubmit").click(function () {
         }
     })
 });
+
+// Submitting a new Area in a City
+$("#newAreaSubmit").click(async function () {
+    let newAreaName = await $("#areaNameAddArea").val();
+    //alert(newAreaName);
+    let cityIdValue = 0;
+
+    //let data = { "name": newCityName };
+    ////data[name] = newCityName;
+    //console.log(data);
+
+    //$.ajax({
+    //    type: "POST",
+    //    url: "http://localhost:1028/api/cities",
+    //    data: JSON.stringify(data),
+    //    contentType: "application/json; charset=utf-8",
+    //    crossDomain: true,
+    //    dataType: "json",
+    //    success: function () {
+    //        alert("success");
+    //    },
+    //    error: function (jqXHR, status) {
+    //        // error handler
+    //        console.log(jqXHR);
+    //        alert('fail' + status.code);
+    //    }
+    //})
+});
+
+async function getCitiesFromDb() {
+    await $.getJSON('http://localhost:1028/api/cities', function (data) {
+        for (var i = 0; i < data.length; i++) {
+            allCities.push(data[i]);
+        }   
+    });
+    populateCities();
+    populateCitiesSelectAddArea();
+    populateCitiesSelectAddParkingLot();
+}
+
+function populateCities() {
+    for (var i = 0; i < allCities.length; i++) {
+        $("#citiesContainer").append(
+            `<p id="city${allCities[i].id}">${allCities[i].name}</p>`
+        );
+    }
+}
+
+function populateCitiesSelectAddArea() {
+    for (var i = 0; i < allCities.length; i++) {
+        let element = `<option value="${allCities[i].id}">${allCities[i].name}</option>`;
+        $("#selectCityAddArea").append(element);
+    }
+    
+}
+
+function populateCitiesSelectAddParkingLot() {
+    for (var i = 0; i < allCities.length; i++) {
+        let element = `<option value="${allCities[i].id}">${allCities[i].name}</option>`;
+        $("#selectCityAddParkingLot").append(element);
+    }
+}
