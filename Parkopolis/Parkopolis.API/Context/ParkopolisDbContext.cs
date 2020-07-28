a602846173d451139cea3f10fe397e8d2383e5bf
Stefan Gherman <scgheman@gmail.com>
2020-07-28 05:55:04 +0300

    Add Db context

---
+++ Parkopolis/Parkopolis.API/Context/ParkopolisDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parkopolis.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parkopolis.API.Context
+{
+    public class ParkopolisDbContext : DbContext
+    {
+        public ParkopolisDbContext(DbContextOptions<ParkopolisDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Area> Areas { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<ParkingLot> ParkingLots { get; set; }
+        public DbSet<ParkingSpace> ParkingSpaces { get; set; }
+    }
+}
