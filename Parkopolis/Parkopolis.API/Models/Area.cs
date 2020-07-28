c0f8201423954f4e2207a7ae09341ce2fe6314aa
Stefan Gherman <scgheman@gmail.com>
2020-07-28 05:54:46 +0300

    Add models for app entities

---
+++ Parkopolis/Parkopolis.API/Models/Area.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parkopolis.API.Models
+{
+    public class Area
+    {
+        public int Id { get; set; }
+        public int CityId { get; set; }
+        public string Name { get; set; }
+
+        //Navigation Properties
+        public  City City { get; set; }
+        public ICollection<ParkingLot> ParkingLots { get; set; }
+    }
+}
---
+++ Parkopolis/Parkopolis.API/Models/ParkingLot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parkopolis.API.Models
+{
+    public class ParkingLot
+    {
+        public int Id { get; set; }
+        public int AreaId { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public bool IsPaid { get; set; }
+        public bool IsStateOwned { get; set; }
+        public int TotalParkingSpaces { get; set; }
+        public bool HasSecurity { get; set; }
+        public int UserId { get; set; }
+        //public DateTime FreeInterval { get; set; }
+
+        //Navigation Properties
+        public Area Area { get; set; }
+        public User User { get; set; }
+        public ICollection<ParkingSpace> ParkingSpaces { get; set; }
+
+    }
+}
