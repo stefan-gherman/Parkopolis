9fe47daad41898bb9c9422f1337da9ea3201c2e4
Dan Dumitriu <dandumitriu33@gmail.com>
2020-07-28 20:08:30 +0300

    Add home page select area based on city

--- Parkopolis/Parkopolis.WEB/wwwroot/js/home.js
+++ Parkopolis/Parkopolis.WEB/wwwroot/js/home.js
@@ -1,5 +1,7 @@
 ﻿populateCitiesDropdown();
-
+$("#selectCityHome").change(function () {
+    populateAreasDropdown();
+});
 
 async function populateCitiesDropdown() {
     await $.getJSON('http://localhost:1028/api/cities', function (data) {
@@ -9,4 +11,17 @@ async function populateCitiesDropdown() {
         }
     });
     
+}
+
+async function populateAreasDropdown() {
+    let cityId = $("#selectCityHome").val();
+    let initialElement = `<option value="" disabled selected hidden>Area</option>`;
+    $("#selectAreaHome").empty();
+    $("#selectAreaHome").append(initialElement);
+    await $.getJSON(`http://localhost:1028/api/cities/${cityId}/areas`, function (data) {
+        for (var i = 0; i < data.length; i++) {
+            let element = `<option value="${data[i].id}">${data[i].name}</option>`;
+            $("#selectAreaHome").append(element);
+        }
+    })
 }
