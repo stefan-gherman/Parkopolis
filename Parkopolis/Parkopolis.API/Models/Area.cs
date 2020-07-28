fcc45f25c3871f34c6516def1837bf870804dc99
Stefan Gherman <scgheman@gmail.com>
2020-07-28 12:12:35 +0300

    Add constraints to fields

--- Parkopolis/Parkopolis.API/Models/Area.cs
+++ Parkopolis/Parkopolis.API/Models/Area.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +9,18 @@ namespace Parkopolis.API.Models
 {
     public class Area
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public int CityId { get; set; }
+       
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
 
         //Navigation Properties
-        public  City City { get; set; }
+        
+        //public City City { get; set; }
+        public int CityId { get; set; }
         public ICollection<ParkingLot> ParkingLots { get; set; }
     }
 }
