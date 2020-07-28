32baa69cacc8cce4d0659cb0cca4b4a884dc2ada
Stefan Gherman <scgheman@gmail.com>
2020-07-28 07:00:31 +0300

    Add repository class for data mangement

---
+++ Parkopolis/Parkopolis.API/Services/ParkopolisDbRepository.cs
@@ -0,0 +1,18 @@
+﻿using Parkopolis.API.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parkopolis.API.Services
+{
+    public class ParkopolisDbRepository : IParkopolisRepository
+    {
+        private readonly ParkopolisDbContext _context;
+
+        public ParkopolisDbRepository(ParkopolisDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
