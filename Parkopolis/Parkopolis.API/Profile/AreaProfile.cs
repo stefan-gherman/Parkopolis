184b3deda29592b6ec6d97ee12d1c76b0c1bfc41
Stefan Gherman <scgheman@gmail.com>
2020-07-28 12:59:09 +0300

    Add AreaProfile for automapper

---
+++ Parkopolis/Parkopolis.API/Profile/AreaProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Parkopolis.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parkopolis.API.Profile
+{
+    public class AreaProfile : AutoMapper.Profile
+    {
+        public AreaProfile()
+        {
+            CreateMap<Models.Area, Models.AreaForDisplayDto>();
+        }
+    }
+
+}
