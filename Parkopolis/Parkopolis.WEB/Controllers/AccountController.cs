d594034447c50b0dfa99d6dc2dc21d898a75be21
Claudiu <cduneata@gmail.com>
2020-07-29 11:01:12 +0300

    Add login get and post request methods

--- Parkopolis/Parkopolis.WEB/Controllers/AccountController.cs
+++ Parkopolis/Parkopolis.WEB/Controllers/AccountController.cs
@@ -20,6 +20,13 @@ namespace Parkopolis.WEB.Controllers
             this.signInManager = signInManager;
         }
 
+        [HttpPost]
+        public async Task<ActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("index", "home");
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
@@ -46,6 +53,33 @@ namespace Parkopolis.WEB.Controllers
                 {
                     ModelState.AddModelError("", error.Description);
                 }
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login (LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+    
+
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+
             }
 
             return View(model);
