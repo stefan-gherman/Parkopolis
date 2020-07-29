4c1739d480eb5016792798625ee504a7c8138091
Claudiu <cduneata@gmail.com>
2020-07-29 14:10:34 +0300

    Add dbset for application user

--- Parkopolis/Parkopolis.API/Context/ParkopolisDbContext.cs
+++ Parkopolis/Parkopolis.API/Context/ParkopolisDbContext.cs
@@ -18,7 +18,9 @@ namespace Parkopolis.API.Context
         public DbSet<City> Cities { get; set; }
         public DbSet<ParkingLot> ParkingLots { get; set; }
         public DbSet<ParkingSpace> ParkingSpaces { get; set; }
+        public override DbSet<ApplicationUser> Users { get; set; }
 
+        
         protected override void OnModelCreating(ModelBuilder model)
         {
             model.Entity<Area>().ToTable("Area");
