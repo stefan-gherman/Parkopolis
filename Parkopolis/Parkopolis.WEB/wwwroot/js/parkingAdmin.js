cccb4ca9ea7cc4574b708366e980f4f1066944c3
Dan Dumitriu <dandumitriu33@gmail.com>
2020-07-30 19:30:01 +0300

    Add feature - Edit Parking Space on Parking Admin

--- Parkopolis/Parkopolis.WEB/wwwroot/js/parkingAdmin.js
+++ Parkopolis/Parkopolis.WEB/wwwroot/js/parkingAdmin.js
@@ -98,6 +98,7 @@ async function populateResultingParkingSapces() {
                                 </p>
                             </div>
                         </div>
+                        <div class="container" id="editParkingSpaceContainer${data[i].id}"></div>
                    </a>`
             $("#parkingSpaceList").append(element);
         }
@@ -155,6 +156,129 @@ async function handleForceFreeParkingSpace(cityId, areaId, parkingLotId, parking
 
 async function handleEditParkingSpace(cityId, areaId, parkingLotId, parkingSpaceId) {
     console.log("Entered Edit process");
+    let largeUrl = `http://localhost:1028/api/cities/${cityId}/areas/${areaId}/parkinglots/${parkingLotId}/parkingspaces/${parkingSpaceId}`;
+
+    // get data about the parking space from DB
+    let parkingSpaceFromDb = {
+        "id": 0,
+        "parkingLotId": 0,
+        "name": "",
+        "isTaken": false,
+        "hasCarWash": false,
+        "isCovered": false,
+        "price": 0.00,
+        "details": ""
+    }
+    await $.getJSON(largeUrl, async function (data) {
+        parkingSpaceFromDb.id = data.id;
+        parkingSpaceFromDb.parkingLotId = data.parkingLotId;
+        parkingSpaceFromDb.name = data.name;
+        parkingSpaceFromDb.isTaken = data.isTaken;
+        parkingSpaceFromDb.hasCarWash = data.hasCarWash;
+        parkingSpaceFromDb.isCovered = data.isCovered;
+        parkingSpaceFromDb.price = data.price;
+        parkingSpaceFromDb.details = data.details;
+    });
+
+    // create the form to edit with populated info
+    $(`#editParkingSpaceContainer${parkingSpaceId}`).empty();
+    let editFormHasCarWashChecked = "";
+    if (parkingSpaceFromDb.hasCarWash == true) {
+        editFormHasCarWashChecked = "checked"
+    }
+    let editFormIsCoveredChecked = "";
+    if (parkingSpaceFromDb.isCovered == true) {
+        editFormIsCoveredChecked = "checked"
+    }
+    let element = `
+                    <form>
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="editParkingSpaceName" value="${parkingSpaceFromDb.name}">
+                            <input type="number" class="form-control" id="editParkingSpacePrice" step=".01" value="${parkingSpaceFromDb.price}">
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="editHasCarWash" ${editFormHasCarWashChecked}>
+                                <label class="form-check-label" for="editHasCarWash">Has Car Wash Service</label>
+                            </div>
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="editHasCover" ${editFormIsCoveredChecked}>
+                                <label class="form-check-label" for="editHasCover">Has Cover (roof or underground)</label>
+                            </div>
+                        </div>
+                        <button id="editParkingSpaceSubmit" type="submit" class="btn btn-primary">Confirm Edit</button>
+                        <button id="editParkingSpaceCancel" type="submit" class="btn btn-primary">Cancel</button>
+                        
+                    </form>
+                    `;
+    $(`#editParkingSpaceContainer${parkingSpaceId}`).append(element);
+    $("#editParkingSpaceName").click(function (event) {
+        event.preventDefault();
+    })
+    $("#editParkingSpacePrice").click(function (event) {
+        event.preventDefault();
+    })
+    $("#editParkingSpaceCancel").click(function (event) {
+        event.preventDefault();
+        $("#editParkingSpaceContainer").empty();
+    })
+    $("#editParkingSpaceSubmit").click(async function (event) {
+        event.preventDefault();
+        parkingSpaceFromDb.name = $("#editParkingSpaceName").val();
+        parkingSpaceFromDb.price = $("#editParkingSpacePrice").val();
+        if ($("input[id='editHasCarWash']:checked").val() === "on") {
+            parkingSpaceFromDb.hasCarWash = true;
+        }
+        if ($("input[id='editHasCover']:checked").val() === "on") {
+            parkingSpaceFromDb.isCovered = true;
+        }
+        await $.ajax({
+            type: "PUT",
+            url: largeUrl,
+            data: JSON.stringify(parkingSpaceFromDb),
+            contentType: "application/json; charset=utf-8",
+            crossDomain: true,
+            dataType: "json",
+            success: function () {
+                console.log("Spot freed-up successfully.");
+            },
+            error: function (jqXHR, status) {
+                // error handler
+                console.log(jqXHR);
+                alert('fail' + status.code);
+            }
+        })
+        // refreshing the Parking Spaces only
+        populateResultingParkingSapces();   
+    })
+
+    
+    //await $.getJSON(largeUrl, async function (data) {
+    //    data =
+    //        [
+    //            {
+    //                "op": "replace",
+    //                "path": "/isTaken",
+    //                "value": false
+    //            }
+    //        ]
+    //    await $.ajax({
+    //        type: "PATCH",
+    //        url: largeUrl,
+    //        data: JSON.stringify(data),
+    //        contentType: "application/json; charset=utf-8",
+    //        crossDomain: true,
+    //        dataType: "json",
+    //        success: function () {
+    //            console.log("Spot freed-up successfully.");
+    //        },
+    //        error: function (jqXHR, status) {
+    //            // error handler
+    //            console.log(jqXHR);
+    //            alert('fail' + status.code);
+    //        }
+    //    })
+    //    // refreshing the Parking Spaces only
+    //    populateResultingParkingSapces();
+    //});
 };
 
 async function handleDeleteParkingSpace(cityId, areaId, parkingLotId, parkingSpaceId) {
