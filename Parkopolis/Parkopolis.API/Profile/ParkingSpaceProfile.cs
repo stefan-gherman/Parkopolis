3e9e4fa9b758fd06e71fc8b3c08839ff0799f767
Stefan Gherman <scgheman@gmail.com>
2020-08-02 15:46:48 +0300

    Add auto mapper profile for parking lots

---
+++ Parkopolis/Parkopolis.API/Profile/ParkingSpaceProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parkopolis.API.Profile
+{
+    public class ParkingSpaceProfile : AutoMapper.Profile
+    {
+        public ParkingSpaceProfile()
+        {
+            CreateMap<Models.ParkingSpace, Models.ParkingSpaceForUpdateDto>().ReverseMap();
+        }
+    }
+}
